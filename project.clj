1f15c7b6612faf038f6ceb106f97089db1f80902
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-07-08 01:53:17 -0400

    Remove swank-clojure from dev-dependencies, folks can use plugin instead

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.eclipse.jgit "0.12.1"]
                  [clj-file-utils "0.2.1"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :repositories {"jgit-repository" "http://download.eclipse.org/jgit/maven"})
