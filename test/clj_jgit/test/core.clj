edb462cd4ea2f351c4c5f20ec0952e70e113c489
vijaykiran <mail@vijaykiran.com>
2012-02-10 00:26:19 +0100

    Utils - move into a single file. - Test for utils method.

--- test/clj_jgit/test/core.clj
+++ test/clj_jgit/test/core.clj
@@ -1,6 +1,13 @@
 (ns clj-jgit.test.core
-  (:use [clj-jgit.porcelain])
+  (:use [clj-jgit.util])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-name-from-uri
+  (let [uris ["ssh://example.com/~/www/project.git"
+              "~/your/repo/path/project.git"
+              "http://git.example.com/project.git"
+              "https://git.example.com/project.git"]]
+    (is (every? #(= %1 "project") (map #(name-from-uri %) uris))
+        "Repository name must be 'project'")))
+
+(deftest )
