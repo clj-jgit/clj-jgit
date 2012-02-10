67494a1add073c156ebbea16ae8962746cdea973
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2012-02-10 14:10:13 -0500

    Bump to v0.0.3, vijaykiran adding features

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "0.0.2"
+(defproject clj-jgit "0.0.3"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.eclipse.jgit "1.2.0.201112221803-r"]]
