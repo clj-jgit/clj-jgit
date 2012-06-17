3b9c98bc151bb4920f9799cfa6c32c536ed64348
Dima Sabanin <sdmitry@gmail.com>
2012-06-17 16:35:23 +0800

    Upgraded to JGit 1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-jgit "0.0.3"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.eclipse.jgit "1.2.0.201112221803-r"]]
+                 [org.eclipse.jgit "1.3.0.201202151440-r"]
                  [org.clojure/core.memoize "0.5.1"]]
   :repositories {"jgit-repository" "http://download.eclipse.org/jgit/maven"})
