8294925950a1daab385d0cf0b198ae2866334a6a
Rob Levy <robert.levy@drakerlabs.com>
2012-09-28 19:47:56 -0400

    macro enabling various unit test

--- test/clj_jgit/test/helpers.clj
+++ test/clj_jgit/test/helpers.clj
@@ -1,4 +1,5 @@
 (ns clj-jgit.test.helpers
+  (:require [fs.core :as fs])
   (:use clj-jgit.porcelain))
 
 ; Using clj-jgit repo for read-only tests
@@ -7,3 +8,15 @@
 (defmacro read-only-repo [& body]
   `(with-repo ~read-only-repo-path
      ~@body))
+
+(defmacro with-tmp-repo
+  "execute operations within a created and immediately deleted repo,
+   producing only the results of expressions in body."
+  [repo-path & body]
+  `(do
+     (fs/delete-dir ~repo-path)
+     (fs/mkdir ~repo-path)
+     (git-init ~repo-path)
+     (let [outcome# (with-repo ~repo-path ~@body)]
+       (fs/delete-dir ~repo-path)
+       outcome#)))
