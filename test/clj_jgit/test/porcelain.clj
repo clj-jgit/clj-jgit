48304b2e8e392337cf58bf8ba3f5aee0947ca13e
Rob Levy <robert.levy@drakerlabs.com>
2012-09-28 19:48:32 -0400

    test current branch (and other) functionality

--- test/clj_jgit/test/porcelain.clj
+++ test/clj_jgit/test/porcelain.clj
@@ -2,7 +2,7 @@
   (:use [clj-jgit.test.helpers]
         [clj-jgit.porcelain]
         [clojure.test])
-  (:import 
+  (:import
     [java.io File]
     [org.eclipse.jgit.api Git]
     [org.eclipse.jgit.revwalk RevWalk]))
@@ -26,3 +26,21 @@
   (read-only-repo
     (is (instance? Git repo))
     (is (instance? RevWalk rev-walk))))
+
+(deftest test-current-branch-functions
+  (is (= [true
+          "master"
+          40
+          false]
+         (with-tmp-repo "target/tmp"
+           (let [tmp-file "target/tmp/tmp.txt"]
+             (spit tmp-file "1")
+             (git-add repo tmp-file)
+             (git-commit repo "first commit")
+             (let [sha (->> repo git-log first str
+                            (re-matches #"^commit ([^\s]+) .*") second)]
+               [(git-branch-attached? repo)
+                (git-branch-current repo)
+                (do (git-checkout repo sha),
+                    (count (git-branch-current repo))) ; char count suggests sha
+                (git-branch-attached? repo)]))))))
