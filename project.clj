0a97c5c7d86d8082b4734e92239993f78d22db85
Dima Sabanin <sdmitry@gmail.com>
2012-10-25 11:56:04 +0800

    Upgrading to JGit 2.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-jgit "0.2"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.eclipse.jgit "1.3.0.201202151440-r"]
-                 [org.clojure/core.memoize "0.5.1"]]
+                 [org.eclipse.jgit "2.1.0.201209190230-r"]
+                 [org.clojure/core.memoize "0.5.2"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]
                      [fs "1.3.2"]
                      [midje "1.4.0"]
