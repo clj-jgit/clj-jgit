3736f50e6247890a932226e0ae7c3374c2189dc7
Dima Sabanin <sdmitry@gmail.com>
2013-03-28 20:49:32 -0400

    Clojure 1.5.1 and JGit 2.3.1

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject clj-jgit "0.2.1"
+(defproject clj-jgit "0.3.1"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.eclipse.jgit "2.1.0.201209190230-r"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.eclipse.jgit "2.3.1.201302201838-r"]
                  [org.clojure/core.memoize "0.5.2"]]
-  :dev-dependencies [[com.stuartsierra/lazytest "1.2.3"]
-                     [fs "1.3.2"]
-                     [midje "1.4.0"]
-                     [lein-marginalia "0.7.1"]
-                     [lein-clojars "0.9.0"]
-                     [lein-midje "1.0.10"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]
+                                  [com.stuartsierra/lazytest "1.2.3"]
+                                  [lein-marginalia "0.7.1"]
+                                  [lein-clojars "0.9.0"]
+                                  [fs "1.3.2"]]
+                   :plugins [[lein-midje "3.0.1"]]}}
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
                  "jgit-repository" "http://download.eclipse.org/jgit/maven"})
