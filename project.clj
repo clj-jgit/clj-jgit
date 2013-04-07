8189c0ec58a72674f60463113f9062c419a275f0
Dima Sabanin <sdmitry@gmail.com>
2013-04-07 18:42:59 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "0.3.2"
+(defproject clj-jgit "0.3.3"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.eclipse.jgit "2.3.1.201302201838-r"]
