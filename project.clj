ffe74fcfd3e322009f37633a22996e4be6e0d446
Dima Sabanin <sdmitry@gmail.com>
2013-04-27 12:01:49 -0400

    fixed marginalia dependency for lein2

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
                  [org.clojure/core.memoize "0.5.2"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
                                   [com.stuartsierra/lazytest "1.2.3"]
-                                  [lein-marginalia "0.7.1"]
                                   [lein-clojars "0.9.0"]
                                   [fs "1.3.2"]]
-                   :plugins [[lein-midje "3.0.1"]]}}
+                   :plugins [[lein-midje "3.0.1"]
+                             [lein-marginalia "0.7.1"]]}}
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
                  "jgit-repository" "http://download.eclipse.org/jgit/maven"})
