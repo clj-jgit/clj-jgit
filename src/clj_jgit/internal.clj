ff73d436b535850f2ebd004f105c19509a305674
Dima Sabanin <sdmitry@gmail.com>
2013-06-16 11:12:44 -0400

    whitespace change

--- src/clj_jgit/internal.clj
+++ src/clj_jgit/internal.clj
@@ -6,11 +6,11 @@
     [org.eclipse.jgit.api Git]
     [org.eclipse.jgit.transport RefSpec]))
 
-(defn ref-spec 
+(defn ref-spec
   ^org.eclipse.jgit.transport.RefSpec [str]
   (RefSpec. str))
 
-(defn new-rev-walk 
+(defn new-rev-walk
   "Creates a new RevWalk instance (mutable)"
   ^org.eclipse.jgit.revwalk.RevWalk [^Git repo]
   (RevWalk. (.getRepository repo)))
@@ -23,30 +23,30 @@
     (.addTree (.getTree rev-commit))
     (.setRecursive true)))
 
-(defn bound-commit 
+(defn bound-commit
   "Find a RevCommit object in a RevWalk and bound to it."
   ^org.eclipse.jgit.revwalk.RevCommit [^Git repo ^RevWalk rev-walk ^ObjectId rev-commit]
   (.parseCommit rev-walk rev-commit))
 
 (defprotocol Resolvable
-  (resolve-object [commit-ish repo] 
+  (resolve-object [commit-ish repo]
     "Find ObjectId instance for any Git name: commit-ish, tree-ish or blob. Accepts ObjectId instances and just passes them through."))
 
 (extend-type String
   Resolvable
-  (resolve-object 
+  (resolve-object
     ^org.eclipse.jgit.lib.ObjectId [^String commit-ish ^Git repo]
     (.resolve (.getRepository repo) commit-ish)))
 
 (extend-type ObjectId
   Resolvable
-  (resolve-object 
+  (resolve-object
     ^org.eclipse.jgit.lib.ObjectId [commit-ish ^Git repo]
     commit-ish))
 
 (extend-type Git
   Resolvable
-  (resolve-object 
+  (resolve-object
     ^org.eclipse.jgit.lib.ObjectId [^Git repo commit-ish]
     "For compatibility with previous implementation of resolve-object, which would take repo as a first argument."
     (resolve-object commit-ish repo)))
