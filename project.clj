009ac4b407612f7fdfd73b68163b1517cd9f2039
Dima Sabanin <sdmitry@gmail.com>
2014-04-22 14:40:35 -0400

    upgraded to JGIT v3.3.2.201404171909-r. Java 7 support only from now on (otherwise no symlinks)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-jgit "0.7.0-alpha1"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit "3.3.1.201403241930-r"]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.3.2.201404171909-r"] 
                  [org.clojure/core.memoize "0.5.3"]
                  [fs "1.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
