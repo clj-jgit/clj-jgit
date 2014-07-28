b5b07463542e6c1cfb8d48f73e8b5df3b1774856
Dima Sabanin <sdmitry@gmail.com>
2014-07-28 17:18:54 -0400

    bumped version 0.7.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "0.7.4"
+(defproject clj-jgit "0.7.5"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.3.2.201404171909-r"] 
                  [org.clojure/core.memoize "0.5.3"]
