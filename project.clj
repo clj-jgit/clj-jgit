9abc3032f368fc151c406ba9394047c81bb0ade2
Dima Sabanin <sdmitry@gmail.com>
2014-11-14 11:16:52 -0500

    Bumped version to 0.8.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "0.8.1"
+(defproject clj-jgit "0.8.2"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.5.0.201409260305-r"]
                  [fs "1.3.2"]]
