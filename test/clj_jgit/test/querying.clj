1484d2660b26bd43c4117c562d45c9e0dcc110ff
Reno Reckling <reno.reckling@mayflower.de>
2015-03-17 19:02:50 +0100

    fix test to not break if there are multiple local branches

--- test/clj_jgit/test/querying.clj
+++ test/clj_jgit/test/querying.clj
@@ -62,49 +62,53 @@
 
   (testing "commit-info"
     (read-only-repo
-      (are 
-        [commit-ish info] (= info (-> commit-ish
-                                    ((partial find-rev-commit repo (new-rev-walk repo))) 
-                                    ((partial commit-info repo))
-                                    (dissoc :repo :raw :time)))
-        "38dd57264cf5c05fb77211c8347d1f16e4474623" {:changed_files 
-                                                    [[".gitignore" :add] 
-                                                     ["README.md" :add] 
-                                                     ["project.clj" :add] 
-                                                     ["src/clj_jgit/core.clj" :add] 
-                                                     ["src/clj_jgit/util/print.clj" :add] 
-                                                     ["test/clj_jgit/test/core.clj" :add]], 
-                                                    :author "Daniel Gregoire", 
-                                                    :email "daniel.l.gregoire@gmail.com", 
-                                                    :message "Initial commit", 
-                                                    :branches ["refs/heads/master"], 
-                                                    :merge false, 
+      (are
+         [commit-ish info] (let [raw-data (-> commit-ish
+                                              ((partial find-rev-commit repo (new-rev-walk repo)))
+                                              ((partial commit-info repo)))
+                                 expected-basic (dissoc raw-data :repo :raw :time :branches)
+                                 info-basic (dissoc info :branches)
+                                 target-branch (-> info :branches (first))]
+                             (and (= expected-basic info-basic)
+                                  (some #(= % target-branch) (:branches raw-data))))
+        "38dd57264cf5c05fb77211c8347d1f16e4474623" {:changed_files
+                                                    [[".gitignore" :add]
+                                                     ["README.md" :add]
+                                                     ["project.clj" :add]
+                                                     ["src/clj_jgit/core.clj" :add]
+                                                     ["src/clj_jgit/util/print.clj" :add]
+                                                     ["test/clj_jgit/test/core.clj" :add]],
+                                                    :author "Daniel Gregoire",
+                                                    :email "daniel.l.gregoire@gmail.com",
+                                                    :message "Initial commit",
+                                                    :branches ["refs/heads/master"],
+                                                    :merge false,
                                                     :id "38dd57264cf5c05fb77211c8347d1f16e4474623"}
-        "edb462cd4ea2f351c4c5f20ec0952e70e113c489" {:changed_files 
-                                                    [["src/clj_jgit/porcelain.clj" :edit] 
-                                                     ["src/clj_jgit/util.clj" :add] 
-                                                     ["src/clj_jgit/util/core.clj" :delete] 
-                                                     ["src/clj_jgit/util/print.clj" :delete] 
-                                                     ["test/clj_jgit/test/core.clj" :edit]], 
-                                                    :author "vijaykiran", 
-                                                    :email "mail@vijaykiran.com", 
-                                                    :message "Utils - move into a single file.\n- Test for utils method.", 
-                                                    :branches ["refs/heads/master"], 
-                                                    :merge false, 
+        "edb462cd4ea2f351c4c5f20ec0952e70e113c489" {:changed_files
+                                                    [["src/clj_jgit/porcelain.clj" :edit]
+                                                     ["src/clj_jgit/util.clj" :add]
+                                                     ["src/clj_jgit/util/core.clj" :delete]
+                                                     ["src/clj_jgit/util/print.clj" :delete]
+                                                     ["test/clj_jgit/test/core.clj" :edit]],
+                                                    :author "vijaykiran",
+                                                    :email "mail@vijaykiran.com",
+                                                    :message "Utils - move into a single file.\n- Test for utils method.",
+                                                    :branches ["refs/heads/master"],
+                                                    :merge false,
                                                     :id "edb462cd4ea2f351c4c5f20ec0952e70e113c489"}
-        "0d3d1c2e7b6c47f901fcae9ef661a22948c64573" {:changed_files 
-                                                    [[".gitignore" :edit] 
-                                                     ["src/clj_jgit/porcelain.clj" :edit] 
-                                                     ["src/clj_jgit/util.clj" :add] 
-                                                     ["src/clj_jgit/util/core.clj" :delete] 
-                                                     ["src/clj_jgit/util/print.clj" :delete] 
-                                                     ["test/clj_jgit/test/core.clj" :edit] 
-                                                     ["test/clj_jgit/test/porcelain.clj" :add]], 
-                                                    :author "Daniel Gregoire", 
-                                                    :email "daniel.l.gregoire@gmail.com", 
-                                                    :message "Merge pull request #2 from vijaykiran/master\n\nInit Tests", 
-                                                    :branches ["refs/heads/master"], 
-                                                    :merge true, 
+        "0d3d1c2e7b6c47f901fcae9ef661a22948c64573" {:changed_files
+                                                    [[".gitignore" :edit]
+                                                     ["src/clj_jgit/porcelain.clj" :edit]
+                                                     ["src/clj_jgit/util.clj" :add]
+                                                     ["src/clj_jgit/util/core.clj" :delete]
+                                                     ["src/clj_jgit/util/print.clj" :delete]
+                                                     ["test/clj_jgit/test/core.clj" :edit]
+                                                     ["test/clj_jgit/test/porcelain.clj" :add]],
+                                                    :author "Daniel Gregoire",
+                                                    :email "daniel.l.gregoire@gmail.com",
+                                                    :message "Merge pull request #2 from vijaykiran/master\n\nInit Tests",
+                                                    :branches ["refs/heads/master"],
+                                                    :merge true,
                                                     :id "0d3d1c2e7b6c47f901fcae9ef661a22948c64573"})))
 
   (testing "changes-for should return nil on invalid commits"
