7817456d55793095fd7dac5de92a0796ded25572
Dima Sabanin <sdmitry@gmail.com>
2015-03-30 11:08:48 -0400

    Bug fix, for when submodule-walk returns a nil in the sequence

--- src/clj_jgit/porcelain.clj
+++ src/clj_jgit/porcelain.clj
@@ -509,6 +509,7 @@
   ([repo]
      (->> (submodule-walk (.getRepository repo) 0)
           (flatten)
+          (filter identity)
           (map #(Git/wrap %))))
   ([repo level]
      (when (< level 3)
