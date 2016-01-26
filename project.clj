366f12edfb2ca07bfc4115d9ad9e86d3fc56291f
Jeffrey Simon <jsimon@yodle.com>
2016-01-26 16:42:32 -0500

    Using my clojars group so I can publish it to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "0.8.8"
+(defproject org.clojars.jaydeesimon/clj-jgit "0.8.8-1"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.7.0.201502260915-r" :exclusions [com.jcraft/jsch]]
                  [fs "1.3.2"]
