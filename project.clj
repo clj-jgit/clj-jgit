4c840f63205af52412ce6c8df15b8fdbf87ede36
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2017-05-23 16:17:35 +0200

    Update deps and fix build

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject clj-jgit "0.8.8"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit.java7 "3.7.0.201502260915-r" :exclusions [com.jcraft/jsch]]
-                 [fs "1.3.2"]
-                 [com.jcraft/jsch "0.1.52"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.8.0.201706111038-r" :exclusions [com.jcraft/jsch]]
+                 [fs "1.3.3"]
+                 [com.jcraft/jsch "0.1.54"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [midje "1.5.1"]
-                                  [com.stuartsierra/lazytest "1.2.3"]
+                                  [com.stuartsierra/lazytest "1.2.3" :exclusions [org.clojure.contrib/find-namespaces
+                                                                                  org.clojure/clojure]]
                                   [lein-clojars "0.9.0"]]} }
   :plugins [[lein-midje "3.0.1"]
             [lein-marginalia "0.7.1"]]
