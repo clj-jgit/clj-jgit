32a8062c15c5e3fcf0e1e5f7d46d7e36d30f5fa6
Peter Monks <pmonks@gmail.com>
2018-03-26 13:20:26 -0700

    :arrow_up: Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-jgit "0.9.1-SNAPSHOT"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.8.0.201706111038-r" :exclusions [com.jcraft/jsch]]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "4.11.0.201803080745-r" :exclusions [com.jcraft/jsch]]
                  [com.jcraft/jsch "0.1.54"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :plugins [[lein-marginalia "0.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :plugins [[lein-marginalia "0.9.1"]]
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
