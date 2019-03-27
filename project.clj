edb9223d0cbddc413c037ac55d5470593a31a4f5
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-03-27 07:01:56 +0100

    Bump upstream jgit version to 5.3.0.201903130848-r

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-jgit "1.0.0-SNAPSHOT"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.1.1.201809181055-r" :exclusions [com.jcraft/jsch]]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.3.0.201903130848-r" :exclusions [com.jcraft/jsch]]
                  [com.jcraft/jsch "0.1.54"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :plugins [[lein-marginalia "0.9.1"]]
