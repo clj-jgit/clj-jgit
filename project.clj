bf5bbdba9e7e635811bb121bbc81a86423e9d2a0
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-07-26 14:11:20 +0200

    Bump upstream deps, add apache mina

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-jgit "1.0.0-SNAPSHOT"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.3.0.201903130848-r" :exclusions [com.jcraft/jsch]]
-                 [com.jcraft/jsch "0.1.54"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.4.0.201906121030-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.4.0.201906121030-r"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :plugins [[lein-marginalia "0.9.1"]]
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
