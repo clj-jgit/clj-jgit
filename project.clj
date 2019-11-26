d3ef50b9d59d72e5c76776eb47e998a5a1ab597f
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-11-26 13:58:18 +0100

    Bump project version to 1.0.0-beta3 and update to latest JGit

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-jgit "1.0.0-beta2"
+(defproject clj-jgit "1.0.0-beta3"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.4.0.201906121030-r"]
-                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.4.0.201906121030-r"]]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.5.1.201910021850-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.5.1.201910021850-r"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :plugins [[lein-marginalia "0.9.1"]]
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
