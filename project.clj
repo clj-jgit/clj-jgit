a5f8130c5f798603965f6fae10cf3f051357373c
Erik Dannenberg <erik.dannenberg@bbe-consulting.de>
2019-11-29 19:11:04 +0100

    Bump project version to 1.0.0-beta4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "1.0.0-beta3"
+(defproject clj-jgit "1.0.0-beta4"
   :description "Clojure wrapper for JGit"
   :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.5.1.201910021850-r"]
                  [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.5.1.201910021850-r"]]
