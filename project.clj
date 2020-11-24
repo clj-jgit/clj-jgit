c26888b75fea68ff75c5d68cb9974557600d70c8
Erik Dannenberg <erik.dannenberg@xtrade-gmbh.de>
2020-11-24 12:28:38 +0100

    Bump jgit deps to 5.9.0.202009080501-r

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-jgit "1.0.1"
   :description "Clojure wrapper for JGit"
-  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.8.1.202007141445-r"]
-                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.8.1.202007141445-r"]
-                 [org.eclipse.jgit/org.eclipse.jgit.gpg.bc "5.8.1.202007141445-r"]]
+  :dependencies [[org.eclipse.jgit/org.eclipse.jgit "5.9.0.202009080501-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit.ssh.apache "5.9.0.202009080501-r"]
+                 [org.eclipse.jgit/org.eclipse.jgit.gpg.bc "5.9.0.202009080501-r"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :plugins [[lein-marginalia "0.9.1"]]
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
