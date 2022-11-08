3f537ecd5810971e774e88946db7d8cdff254f4a
Erik Dannenberg <erik.dannenberg@xtrade-gmbh.de>
2022-11-08 12:11:06 +0100

    Bump jgit deps to 6.4.0.202211300538-r

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-jgit "1.1.0-SNAPSHOT"
   :description "Clojure wrapper for JGit"
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}}
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :plugins [[lein-marginalia "0.9.1"]
             [lein-tools-deps "0.4.5"]]
