09539ced91d46c7500da1ec6e6ce78b1f2218997
Erik Dannenberg <erik.dannenberg@xtrade-gmbh.de>
2022-11-08 12:09:19 +0100

    Bump project version to 1.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jgit "1.0.2"
+(defproject clj-jgit "1.1.0-SNAPSHOT"
   :description "Clojure wrapper for JGit"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
@@ -6,4 +6,3 @@
             [lein-tools-deps "0.4.5"]]
   :lein-tools-deps/config {:config-files [:install :user :project]}
   :repositories {"jgit-repository" "https://repo.eclipse.org/content/groups/releases/"})
-
